1c06034581607a348f7a9837abb7fff69760aaf4
Walter Cojal <walter_cojal@hotmail.com>
2022-03-20 09:14:09 -0500

    Add fragments and nav_host_fragment

---
+++ app/src/main/java/com/android/cedecsi/navigation/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.android.cedecsi.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.cedecsi.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}

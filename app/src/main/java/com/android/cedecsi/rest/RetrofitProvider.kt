f45b875fa738b19e4d133f68489364dbd5111027
Walter Cojal <walter_cojal@hotmail.com>
2022-03-19 20:26:43 -0500

    Add loggin interceptor

--- app/src/main/java/com/android/cedecsi/rest/RetrofitProvider.kt
+++ app/src/main/java/com/android/cedecsi/rest/RetrofitProvider.kt
@@ -4,6 +4,7 @@ import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -21,6 +22,12 @@ class RetrofitProvider {
 
         private fun provideHttpClient(): OkHttpClient {
             val builder = OkHttpClient.Builder()
+
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+
+            builder.addInterceptor(logging)
+
             return builder.build()
         }
 
